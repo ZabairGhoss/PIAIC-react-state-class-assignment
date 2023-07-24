6e31479b871ed0f8be9436592b28b1b45dc1ef06
Zabair Ghoss <czubair307@gmail.com>
2023-07-24 23:54:32 +0500

    React TodoApp component added

---
+++ src/app/components/TodoApp.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React, { useState } from "react";
+
+const TodoApp = () => {
+  const [input, setInput] = useState("");
+
+  return (
+    <div className="w-1/2 bg-gray-300 rounded-md py-10">
+      <h1 className="text-center font-bold text-2xl">React TodoApp</h1>
+      <div className="flex">
+        <input type="text" placeholder="type here" className="" />
+        <button>Add</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoApp;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Practice from './components/Practice'
+import TodoApp from './components/TodoApp'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Practice />
+        <TodoApp />
     </main>
   )
 }
